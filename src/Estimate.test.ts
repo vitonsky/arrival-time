f7271e00ed9fbb0b3249f28cdb0b41fea47c0042
Robert Vitonsky <rob@vitonsky.net>
2024-08-04 00:31:52 +0200

    test: add basic cases

--- src/Estimate.test.ts
+++ src/Estimate.test.ts
@@ -1,6 +1,127 @@
 import { Estimate } from '.';
 
-test('basic test', () => {
-	const estimate = new Estimate();
-	expect(estimate.update(50, 100).speed).toBeGreaterThan(50);
+const createCounter = ({
+	value = 0,
+	step = 1,
+}: { step?: number; value?: number } = {}) => {
+	let counter = value;
+	return () => {
+		counter += step;
+		return counter;
+	};
+};
+
+describe('basic usage', () => {
+	test('4 steps to complete', () => {
+		const timerMock = createCounter();
+		const estimate = new Estimate({ timeFetcher: timerMock });
+
+		expect(estimate.update(25, 100)).toEqual(
+			expect.objectContaining({
+				timeDelta: 1,
+				estimate: 3,
+				progressLeft: 75,
+			}),
+		);
+
+		expect(estimate.update(50, 100)).toEqual(
+			expect.objectContaining({
+				timeDelta: 2,
+				estimate: 2,
+				progressLeft: 50,
+			}),
+		);
+
+		expect(estimate.update(75, 100)).toEqual(
+			expect.objectContaining({
+				timeDelta: 3,
+				estimate: 1,
+				progressLeft: 25,
+			}),
+		);
+
+		expect(estimate.update(100, 100)).toEqual(
+			expect.objectContaining({
+				timeDelta: 4,
+				estimate: 0,
+				progressLeft: 0,
+			}),
+		);
+	});
+
+	test('big numbers in progress and total', () => {
+		const timerMock = createCounter();
+		const estimate = new Estimate({ timeFetcher: timerMock });
+
+		expect(estimate.update(25_000_000, 100_000_000)).toEqual(
+			expect.objectContaining({
+				timeDelta: 1,
+				estimate: 3,
+				progressLeft: 75_000_000,
+			}),
+		);
+
+		expect(estimate.update(50_000_000, 100_000_000)).toEqual(
+			expect.objectContaining({
+				timeDelta: 2,
+				estimate: 2,
+				progressLeft: 50_000_000,
+			}),
+		);
+
+		expect(estimate.update(75_000_000, 100_000_000)).toEqual(
+			expect.objectContaining({
+				timeDelta: 3,
+				estimate: 1,
+				progressLeft: 25_000_000,
+			}),
+		);
+
+		expect(estimate.update(100_000_000, 100_000_000)).toEqual(
+			expect.objectContaining({
+				timeDelta: 4,
+				estimate: 0,
+				progressLeft: 0,
+			}),
+		);
+	});
+
+	test('reset call increase speed', () => {
+		const timerMock = createCounter();
+		const estimate = new Estimate({ timeFetcher: timerMock });
+
+		expect(estimate.update(25, 100)).toEqual(
+			expect.objectContaining({
+				timeDelta: 1,
+				speed: 25_000,
+				progressLeft: 75,
+			}),
+		);
+
+		expect(estimate.update(50, 100)).toEqual(
+			expect.objectContaining({
+				timeDelta: 2,
+				speed: 25_000,
+				progressLeft: 50,
+			}),
+		);
+
+		estimate.reset();
+
+		expect(estimate.update(75, 100)).toEqual(
+			expect.objectContaining({
+				timeDelta: 1,
+				speed: 75_000,
+				progressLeft: 25,
+			}),
+		);
+
+		expect(estimate.update(100, 100)).toEqual(
+			expect.objectContaining({
+				timeDelta: 2,
+				speed: 50_000,
+				progressLeft: 0,
+			}),
+		);
+	});
 });
