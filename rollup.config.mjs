fd74c9444a6eafacc12099817ab002b4b45c2bc7
Robert Vitonsky <rob@vitonsky.net>
2024-08-04 00:58:01 +0200

    build: setup rollup

---
+++ rollup.config.mjs
@@ -0,0 +1,46 @@
+import copy from 'rollup-plugin-copy';
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+	input: 'src/index.ts',
+	output: {
+		dir: 'dist',
+		format: 'cjs',
+	},
+	plugins: [
+		typescript(),
+		copy({
+			targets: [
+				{
+					src: 'LICENSE',
+					dest: 'dist',
+				},
+				{
+					src: 'README.md',
+					dest: 'dist',
+				},
+				{
+					src: 'package.json',
+					dest: 'dist',
+					transform(content) {
+						const ignoredProps = ['devDependencies', 'scripts'];
+
+						const json = JSON.parse(content.toString());
+						return JSON.stringify(
+							{
+								...Object.fromEntries(
+									Object.entries(json).filter(
+										([key]) => !ignoredProps.includes(key),
+									),
+								),
+								main: 'index.js',
+							},
+							null,
+							4,
+						);
+					},
+				},
+			],
+		}),
+	],
+};
